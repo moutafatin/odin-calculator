374a8968d6280fb34779a9cd4d0fe6e23c85885a
Oussama Moutafatin <oussama@moutafatin.com>
2025-06-04 20:44:27 +0100

    Cleanup and refactor

--- src/main.ts
+++ src/main.ts
@@ -1,156 +1,151 @@
-import './style.css'
-import { operate, type Operand } from "./calculator"
+import "./style.css";
+import { operate, type Operand } from "./calculator";
 
+type Token =
+  | { type: "number"; value: number }
+  | { type: "operator"; value: string };
 
-type Token = { type: 'number', value: number } | { type: 'operator', value: string }
+let runningOperation: Token[] = [];
+let justEvaluated = false;
 
-let runningOperation: Token[] = []
-let justEvaluated = false
+const digitBtns = document.querySelectorAll(
+  ".digit-btn"
+) as NodeListOf<HTMLButtonElement>;
+const operationBtns = document.querySelectorAll(
+  ".operation-btn"
+) as NodeListOf<HTMLButtonElement>;
+const resultBtn = document.querySelector(".result-btn") as HTMLButtonElement;
+const resultElm = document.querySelector(
+  ".calculator__result"
+) as HTMLSpanElement;
 
+const clearBtn = document.querySelector(".clear-btn") as HTMLButtonElement;
+const backspaceBtn = document.querySelector(
+  ".backspace-btn"
+) as HTMLButtonElement;
 
+const currentOperationDisplay = document.querySelector(
+  ".current-operation"
+) as HTMLSpanElement;
 
-const digitBtns = document.querySelectorAll(".digit-btn") as NodeListOf<HTMLButtonElement>
-const operationBtns = document.querySelectorAll(".operation-btn") as NodeListOf<HTMLButtonElement>
-const resultBtn = document.querySelector(".result-btn") as HTMLButtonElement
-const resultElm = document.querySelector(".calculator__result") as HTMLSpanElement
+clearBtn.addEventListener("click", handleClear);
 
-const clearBtn = document.querySelector(".clear-btn") as HTMLButtonElement
-const backspaceBtn = document.querySelector(".backspace-btn") as HTMLButtonElement
+backspaceBtn.addEventListener("click", handleBackspace);
 
-clearBtn.addEventListener("click", handleClear)
-backspaceBtn.addEventListener("click", handleBackspace)
+resultBtn.addEventListener("click", handleResultClick);
 
+let buffer = "";
 
-function handleClear() {
-  runningOperation = []
-  buffer = ""
-  resultElm.textContent = "0"
-  justEvaluated = false
-  updateCurrentOperationDisplay()
+digitBtns.forEach((digitBtn) => {
+  digitBtn.addEventListener("click", handleDigitClick);
+});
+
+function handleDigitClick(e: MouseEvent) {
+  const btn = e.currentTarget as HTMLButtonElement;
+  buffer += btn.textContent;
+
+  updateDisplay();
 }
 
-function handleBackspace() {
-  if (justEvaluated) return
+function getCalculationText() {
+  return runningOperation.reduce((text, opr) => text + ` ${opr.value} `, "");
+}
 
-  if (runningOperation.length === 2 && !buffer) {
-    runningOperation.pop()
-    currentOperationDisplay.textContent = getCalculationText() + buffer
-    return
+function updateDisplay() {
+  currentOperationDisplay.textContent = getCalculationText() + buffer;
+}
+
+function updateCurrentOperationDisplay() {
+  currentOperationDisplay.textContent = getCalculationText();
+}
+
+operationBtns.forEach((operationBtn) => {
+  operationBtn.addEventListener("click", handleOperationClick);
+});
+
+function evaluate(calculation: Token[]) {
+  const [a, op, b] = calculation;
+
+  if (a.type !== "number" || op.type !== "operator" || b.type !== "number") {
+    alert("SOMETHING WRONG HERE");
+    return 0;
   }
 
-  buffer = buffer.slice(0, -1)
-
-  currentOperationDisplay.textContent = getCalculationText() + buffer
-
+  return operate(op.value as Operand, a.value, b.value);
 }
 
-const currentOperationDisplay = document.querySelector(".current-operation") as HTMLSpanElement
-resultBtn.addEventListener("click", () => {
-  if (buffer) {
-    runningOperation.push({ type: "number", value: +buffer })
-    buffer = ""
+function handleOperationClick(e: MouseEvent) {
+  const btn = e.currentTarget as HTMLButtonElement;
 
+  if (!buffer && runningOperation.length === 0) return;
+
+  if (justEvaluated) {
+    justEvaluated = false;
+    runningOperation.push({ type: "operator", value: btn.textContent! });
+    updateCurrentOperationDisplay();
+    return;
+  }
+
+  if (buffer) {
+    runningOperation.push({ type: "number", value: +buffer });
+    buffer = "";
+
+    if (runningOperation.length === 3) {
+      runningOperation = [
+        { type: "number", value: evaluate(runningOperation) },
+      ];
+    }
+
+    runningOperation.push({ type: "operator", value: btn.textContent! });
+    updateCurrentOperationDisplay();
+    return;
+  }
+
+  const last = runningOperation[runningOperation.length - 1];
+  if (last?.type === "operator") {
+    runningOperation.pop();
+  }
+  runningOperation.push({ type: "operator", value: btn.textContent! });
+
+  console.log(runningOperation);
+  updateCurrentOperationDisplay();
+}
+
+function handleResultClick() {
+  if (buffer) {
+    runningOperation.push({ type: "number", value: +buffer });
+    buffer = "";
   }
 
   if (runningOperation.length === 3) {
-    resultElm.textContent = evaluate(runningOperation).toString()
-    updateCurrentOperationDisplay()
-    runningOperation = [{ type: 'number', value: evaluate(runningOperation) }]
+    const result = evaluate(runningOperation);
+    resultElm.textContent = result.toString();
+    updateCurrentOperationDisplay();
+    runningOperation = [{ type: "number", value: result }];
 
-    justEvaluated = true
+    justEvaluated = true;
+  }
+}
+
+function handleClear() {
+  runningOperation = [];
+  buffer = "";
+  resultElm.textContent = "0";
+  justEvaluated = false;
+  updateCurrentOperationDisplay();
+}
+
+function handleBackspace() {
+  if (justEvaluated) return;
+
+  if (runningOperation.length === 2 && !buffer) {
+    runningOperation.pop();
+    updateDisplay();
+
+    return;
   }
 
+  buffer = buffer.slice(0, -1);
 
-})
-
-
-
-let buffer = ""
-
-digitBtns.forEach(digitBtn => {
-  digitBtn.addEventListener("click", (e) => {
-    const btn = e.currentTarget as HTMLButtonElement
-    buffer += btn.textContent
-
-
-    currentOperationDisplay.textContent = getCalculationText() + buffer
-
-  })
-})
-
-
-function getCalculationText() {
-  let text = ""
-
-  runningOperation.forEach(opr => {
-    text += `${opr.value} `
-  })
-  return text
+  updateDisplay();
 }
-
-
-
-
-function updateCurrentOperationDisplay() {
-  currentOperationDisplay.textContent = getCalculationText()
-
-}
-
-
-
-
-
-operationBtns.forEach(operationBtn => {
-  operationBtn.addEventListener("click", (e) => {
-    const btn = e.currentTarget as HTMLButtonElement
-
-    if (!buffer && runningOperation.length === 0) return
-
-    if (justEvaluated) {
-      justEvaluated = false
-      runningOperation.push({ type: 'operator', value: btn.textContent! })
-      updateCurrentOperationDisplay()
-      return
-    }
-
-    if (buffer) {
-      runningOperation.push({ type: "number", value: +buffer })
-      buffer = ""
-
-      if (runningOperation.length === 3) {
-        runningOperation = [{ type: 'number', value: evaluate(runningOperation) }]
-      }
-
-      runningOperation.push({ type: 'operator', value: btn.textContent! })
-      updateCurrentOperationDisplay()
-      return
-    }
-
-
-    const last = runningOperation[runningOperation.length - 1]
-    if (last?.type === 'operator') {
-      runningOperation.pop()
-    }
-    runningOperation.push({ type: 'operator', value: btn.textContent! })
-
-    console.log(runningOperation)
-    updateCurrentOperationDisplay()
-
-
-  })
-})
-
-
-function evaluate(calculation: Token[]) {
-  const [a, op, b] = calculation
-
-  if (a.type !== 'number' || op.type !== 'operator' || b.type !== 'number') {
-    alert("SOMETHING WRONG HERE")
-    return 0
-  }
-
-  return operate(op.value as Operand, a.value, b.value)
-}
-
-
-
