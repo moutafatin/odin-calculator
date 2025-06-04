bb8b5887e369591c20fcdf4741f1786bc0499273
Oussama Moutafatin <oussama@moutafatin.com>
2025-06-04 17:01:08 +0100

    Add UI and simple operation of two operands
    The calculator can't do a slightly complex calculation like 1 + 1 + 1.
    It only works on two operands x + z = y.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,49 @@
 import './style.css'
+import { operate, type Operand } from "./calculator"
 
 
 
 
+const digitBtns = document.querySelectorAll(".digit-btn") as NodeListOf<HTMLButtonElement>
+const operationBtns = document.querySelectorAll(".operation-btn") as NodeListOf<HTMLButtonElement>
+const resultBtn = document.querySelector(".result-btn") as HTMLButtonElement
+const resultElm = document.querySelector(".calculator__result") as HTMLSpanElement
+resultBtn.addEventListener("click", () => {
+
+  const result = operate(operation as Operand, +firstOperand, +secondOperand).toString()
+  resultElm.textContent = result
+  firstOperand = result
+  operation = ""
+  secondOperand = ""
+})
+
+let firstOperand = ""
+let secondOperand = ""
+let operation = ""
+
+digitBtns.forEach(digitBtn => {
+  digitBtn.addEventListener("click", (e) => {
+    const btn = e.currentTarget as HTMLButtonElement
+
+    if (!operation) {
+      firstOperand += btn.textContent
+
+    } else {
+      secondOperand += btn.textContent
+
+    }
+
+    console.log(firstOperand, secondOperand, operation)
+  })
+})
+
+
+
+operationBtns.forEach(operationBtn => {
+  operationBtn.addEventListener("click", (e) => {
+    const btn = e.currentTarget as HTMLButtonElement
+
+    operation = btn.textContent ?? ""
+
+  })
+})
